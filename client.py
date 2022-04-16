846f608a02ee9fc80b1baf865d1e34d7315b7f10
Obsidiam <lukasbownik99@gmail.com>
2022-04-16 22:38:48 +0200

    Basic implementation working

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
+from multiprocessing import Condition, Event
 import socket
 import time
 import threading
 
-target_host = "127.0.0.1"
+target_host = "192.168.1.203"
 target_port = 27700
 msgs = [
     "HELLO-FROM %s",
@@ -10,17 +11,61 @@ msgs = [
 ]
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
+lock = Condition()
+logged_in = False
+event = threading.Event()
 
 def hello(username : str):
     client.send(bytes(msgs[0] % username, 'UTF-8'))
-    response = client.recv(1024)
-    return response.decode('UTF-8')
+
+def send_message_to(msg : str, to : str):
+    client.sendall(bytes("SEND %s %s" % (to, msg), "UTF-8"))
+    
+def recv(client : socket.socket, event : Event):
+    while not event.is_set():
+        lock.acquire()
+        global logged_in
+        response = client.recv(4096)
+        if response:
+            decoded_response = response.decode("UTF-8")
+            if decoded_response.startswith("HELLO"):
+                print("Logged in as %s" % decoded_response.split(" ")[1])
+                logged_in = True
+            if decoded_response == "IN-USE":
+                print("This name is already taken")
+            if decoded_response.startswith("DELIVERY"):
+                chunks = response.decode("UTF-8").split(" ")
+                body = ''
+                for i in range(2, len(chunks)):
+                    body += ' ' + chunks[i]
+                print(body.lstrip())
+            lock.notify_all()
+            lock.release()
+
 
 def run():
-    i = str(input("Input: "))
-    print(hello(i))
-    #client.close()
+    global logged_in
+    while not logged_in:
+        i = str(input("Your name: "))
+        hello(i)
+        time.sleep(1)
+    cmd = ""
+    user = ""
+    print("At any time, !quit to quit")
+    user = str(input("User name:"))
+
+    while cmd != "!quit":
+        cmd = str(input(">"))
+        if cmd and cmd != "!quit":
+            send_message_to(cmd, user)
+            cmd = ''
+        else:
+            #client.close()
+            event.set()
+            exit(0)
 
 if __name__ == '__main__':
+    client_handler = threading.Thread(target = recv, args=(client,event,))
+    client_handler.start()
     run()
     
--- server.py
+++ server.py
@@ -1,16 +1,18 @@
+from concurrent.futures import thread
 from http import server
-from re import S
 import socket
 import threading
 import uuid
+import time
 
 class Session():
     def __init__(self, ip : str):
         self.__sid = uuid.uuid4()
         self.__ip = ip
         self.__socket = None
+        self.__username = ""
     
-    def assing_user(self, username : str):
+    def assign_user(self, username : str):
         self.__username = username
 
     def assing_socket(self, socket : socket.socket):
@@ -36,15 +38,15 @@ class Response():
         return bytes(self.__payload, enc)
 
 class Command():
-    def __init__(self, msg : str, func : callable):
-        self.msg = msg
+    def __init__(self, func : callable, body_start_index : int):
         self.func = func
+        self.__body_start_index = body_start_index
 
-    def as_text(self) -> str:
-        return self.msg
-
-    def exec(self, args : list = [], session_queue : list = [], opt_args = []) -> tuple[Response, socket.socket]:
-        return self.func(self, args, session_queue, opt_args)
+    def body_start_index(self):
+        return self.__body_start_index
+    
+    def exec(self, args : list = [], session_queue : list = [], opt_args = []) -> tuple[Response, socket.socket, Response]:
+        return self.func(args, session_queue, opt_args)
 
 class FunctionSet():
     def create_session_if_needed(session_queue : list[Session], ip : str, socket : socket.socket):
@@ -55,30 +57,59 @@ class FunctionSet():
             session_queue.append(s)
 
     @staticmethod
-    def on_hello(command : Command, args : list, session_queue : list[Session], opt_args = []) -> tuple[Response, socket.socket]:
-        r = Response(command.as_text() + " " + args[0])
-        session : Session = list(filter(lambda s : (s.ip() == opt_args[0]), session_queue))[0]
-        session.assing_user(args[0])
-        return (r, session.socket())
+    def on_hello(args : list, session_queue : list[Session], opt_args = []) -> tuple[Response, socket.socket, Response]:
+        r = Response("HELLO " + args[0])
+        session : Session = list(filter(lambda s : (s.username() == args[0]), session_queue))
+        if session:
+            r = Response("IN-USE")
+            return (r, None, None)
+        sessions: list[Session] = list(filter(lambda s : (s.ip() == opt_args[0]), session_queue))
+        if sessions:
+            sessions[0].assign_user(args[0])
+            return (r, sessions[0].socket(), None)
+        else:
+            return (None, None, None)
 
     @staticmethod
-    def on_send_ok(command : Command, args : list,  session_queue : list, opt_args = []) -> tuple[Response, socket.socket]:
-        session : Session = list(filter(lambda s : (s.ip() == opt_args[0]), session_queue))[0]
-        return (Response(command.as_text()), session.socket())
+    def on_send_ok(args : list,  session_queue : list, opt_args = []) -> tuple[Response, socket.socket, Response]:
+        sessions : list[Session] = list(filter(lambda s : (s.username() == args[0]), session_queue))
+        print(args[0])
+        if sessions:
+            r = Response("SEND-OK")
+            r2 = Response("DELIVERY %s %s" % (args[0], args[1]))
+            return (r, sessions[0].socket(), r2)
+        else:
+            print("sessions empty")
+            return (None, None, None)
 
 class CommandParser():
     @staticmethod
-    def parse_command(command_text : str) -> list:
-        return command_text.rstrip().split(" ")
-
+    def parse_command(command_text : str, body_start_index : int = 2) -> list:
+        chunks = command_text.rstrip().split(" ")
+        parsed_command = []
+        if body_start_index != -1:
+            for i in range(0, body_start_index):
+                parsed_command.append(chunks[i])
+            body = ''
+            for i in range(body_start_index, len(chunks)):
+                body += ' ' + chunks[i]
+            parsed_command.append(body.lstrip())
+        else:
+            parsed_command = chunks
+        return parsed_command
+    
+    @staticmethod
+    def find_command(command_text, command_set : list) -> Command:
+        return command_set[command_text.rstrip().split(" ")[0]]
+    
 class Server():
     def __init__(self, ip : str = "127.0.0.1", port : int = 27700):
         self.__bind_ip = ip
         self.__bind_port = port
         self.session_queue = []
         self.command_set = {
-            "HELLO-FROM" : Command("HELLO", FunctionSet.on_hello),
-            "SEND" : Command("SEND-OK", FunctionSet.on_send_ok)
+            "HELLO-FROM" : Command(FunctionSet.on_hello, -1),
+            "SEND" : Command(FunctionSet.on_send_ok, 2)
         }
         self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__server.bind((self.__bind_ip, self.__bind_port))
@@ -87,31 +118,62 @@ class Server():
 
     def __handle_client(self, client_socket : socket.socket):
         while True:
-            request = client_socket.recv(1024)
-            if not request:
-                continue
-            print("[*] Received: %s" % request)
-            parsed_command = CommandParser.parse_command(request.decode("UTF-8"))
-            command = self.command_set[parsed_command[0]]
-            args = []
-            for arg_index in range(1, len(parsed_command)):
-                args.append(parsed_command[arg_index])
-            response, socket = command.exec(args, self.session_queue, [client_socket.getpeername()[0]])
-            print(response)
-            print(socket)
-            socket.send(bytes(response.to_str()))
+            try:
+                request = client_socket.recv(4096)
+                if not request:
+                    continue
+                print("[*] Received: %s on %s" % (request, threading.current_thread().name))
+                command = CommandParser.find_command(request.decode("UTF-8"), self.command_set)
+                parsed_command = CommandParser.parse_command(request.decode("UTF-8"), command.body_start_index())
+                args = []
+                for arg_index in range(1, len(parsed_command)):
+                    args.append(parsed_command[arg_index])
+                response, client_socket2, return_response = command.exec(args, self.session_queue, [client_socket.getpeername()[0]])
+                print("[*] Sending back %s on %s" % (response.to_str(), threading.current_thread().name))
+                client_socket.sendall(bytes(response.to_str()))
+                if client_socket2 and return_response:
+                    print("[*] Sending %s on %s" % (return_response.to_str(), threading.current_thread().name))
+                    client_socket2.send(bytes(return_response.to_str()))
+            except Exception as e:
+                print(threading.current_thread().name)
+                print(e.args)
+                if client_socket2 and type(client_socket2) is socket.socket:
+                    session : Session = list(filter(lambda s : (s.ip() == client_socket2.getpeername()[0]), self.session_queue))[0]
+                    self.session_queue.remove(session)
+                    client_socket2.close()
+                elif client_socket2:
+                    del client_socket2
+                client_socket.close()
+                return
+    def __socket_queue_cleaner(self):
+        while True:
+            for session in self.session_queue:
+                if session:
+                    s = session.socket()
+                    if s:
+                        try:
+                            if type(s) is socket.socket:
+                                s.sendall(bytes("PING", "UTF-8"))     
+                            time.sleep(2)
+                        except:
+                            print("Cleaning... session of %s" % session.username())
+                            del s
+                            self.session_queue.remove(session)
+                            del session
 
     def launch(self):
         while True:
+            print("[*] There are alredy %d thread active" % threading.active_count())
             client, addr = self.__server.accept()
             FunctionSet.create_session_if_needed(self.session_queue, client.getpeername()[0], client)
-
             print("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
             client_handler = threading.Thread(target = self.__handle_client, args=(client,))
+            client_handler.start()
+            client_handler = threading.Thread(target = self.__socket_queue_cleaner)
             client_handler.start()
 
 
 if __name__ == '__main__':
-    server = Server()
+    server = Server('0.0.0.0')
     server.launch()
     
